cc8aa08f4bf8ce7430efb7505db3a672e9db9fea
Laurent <lyenpon974@gmail.com>
2022-03-20 01:11:21 +0100

    Update - Fonts generation

--- _assets/gulp/fonts.js
+++ _assets/gulp/fonts.js
@@ -2,10 +2,9 @@ const { src, series, dest } = require("gulp");
 const paths = require("./paths");
 const gutil = require("gulp-util");
 const rename = require("gulp-rename");
-const browserSync = require("browser-sync");
 
 function fontsBuild() {
-  return src(paths.sassFiles + "/main.scss")
+  return src(paths.fontFiles + "/**/*.*")
     .pipe(
       rename(function (path) {
         path.dirname = "";
@@ -13,7 +12,6 @@ function fontsBuild() {
     )
     .pipe(dest(paths.jekyllFontFiles))
     .pipe(dest(paths.siteFontFiles))
-    .pipe(browserSync.stream())
     .on("error", gutil.log);
 }
 
